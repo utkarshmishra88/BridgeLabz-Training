894d404087be335c6824dc3ede73e6ccc3caaa2c
utkarshmishra88 <utkarshmishra8871@gmail.com>
2025-12-17 12:08:09 +0530

    [Utkarsh] : added average percent mark computation program

---
+++ gcr-codebase/programming-elements/Level1/AveragePercentMark.java
@@ -0,0 +1,15 @@
+class AveragePercentMark{
+public static void main(String[] args) {
+
+    // Marks obtained by Sam in PCM
+    int maths = 94;
+    int physics = 95;
+    int chemistry = 96;
+
+    // Calculating average percentage
+    double average = (maths + physics + chemistry) / 3.0;
+
+    // Printing the result
+    System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
