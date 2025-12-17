14f7a19beb935ce7f08d8fdf5daa69a2e30ea1cd
utkarshmishra88 <utkarshmishra8871@gmail.com>
2025-12-17 11:58:05 +0530

    [Utkarsh] : added age computation program

---
+++ gcr-codebase/programming-elements/Level1/AgeComputation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class AgeComputation
+{
+public static void main(String args[])
+{
+String name="Harry";
+// Birth year of Harry
+int birthYear=2000;
+ // Current year
+int currentYear=2024;
+ // Calculating age
+int age=currentYear-birthYear;
+// Printing the result
+System.out.println("Harry's age in "+currentYear+" is "+age);
+}
+}
