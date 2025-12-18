4b19f135a95b0521f8ccbb28a2480dba17ac09d9
utkarshmishra88 <utkarshmishra8871@gmail.com>
2025-12-18 23:17:19 +0530

    [Utkarsh] : Implemented fizzbuzz logic using while

---
+++ gcr-codebase/control-flows/Level2/FizzBuzzWhile.java
@@ -0,0 +1,35 @@
+//Fizzbuzz java program using while loop.
+import java.util.Scanner;
+
+public class FizzBuzzWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        // Check for positive integer
+        if (n <= 0) {
+            System.out.println("Please enter a positive integer.");
+            return;
+        }
+
+        // FizzBuzz logic using while loop
+        int i = 1;
+        while (i <= n) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+        sc.close();
+    }
+}
