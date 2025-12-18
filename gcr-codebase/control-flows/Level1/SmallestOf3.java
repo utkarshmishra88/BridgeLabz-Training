e1b511b6a2c75fefa7e2d111e7b11e92e75bb6ec
utkarshmishra88 <utkarshmishra8871@gmail.com>
2025-12-18 13:22:05 +0530

    [Utkarsh] : Check if the first is the smallest of the 3 numbers

--- gcr-codebase/control-flows/Level1/SmallestOf3.java
+++ gcr-codebase/control-flows/Level1/SmallestOf3.java
@@ -1,10 +1,8 @@
 //Program to check if the first is the smallest of the 3 numbers.
-
 import java.util.Scanner;
 
 public class SmallestOf3{
-
-public static void main (String args[]){
+  public static void main (String args[]){
    //Create a Scanner Object
    Scanner input = new Scanner(System.in);
    
@@ -14,7 +12,7 @@ public static void main (String args[]){
    int number2=input.nextInt();
    int number3=input.nextInt();
    
-   //Print the result by checking the divisibility
+   //Print the result by checking the smallest first.
    System.out.println("Is the first number the smallest? "+((number1<number2)&&(number1<number3)));
    
    // Closing the Scanner Stream
