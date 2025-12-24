ba11adffc7f69104cc83d9f9f958d0960a253709
utkarshmishra88 <utkarshmishra8871@gmail.com>
2025-12-24 13:09:54 +0530

    [Utkarsh] : Added Arithmetic operation in date

---
+++ gcr-codebase/Extras/Extra/ArithmeticDate.java
@@ -0,0 +1,29 @@
+// Program to perform date arithmetic operations
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class ArithmeticDate {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Take date input from user
+        System.out.print("Enter date (yyyy-mm-dd): ");
+        LocalDate date = LocalDate.parse(input.nextLine());
+
+        // Perform date operations
+        LocalDate resultDate = date
+                .plusDays(7)
+                .plusMonths(1)
+                .plusYears(2)
+                .minusWeeks(3);
+
+        // Display result
+        System.out.println("Final Date after operations: " + resultDate);
+        
+		// Closing scanner stream
+        input.close();
+    }
+}
