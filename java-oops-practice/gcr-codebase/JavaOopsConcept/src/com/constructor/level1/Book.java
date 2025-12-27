2e1a4d7e2aaa6110defe98422247a9d7e70a9c69
utkarshmishra88 <utkarshmishra8871@gmail.com>
2025-12-27 20:24:20 +0530

    [Utkarsh] : Added Book class and BookDriver

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/Book.java
@@ -0,0 +1,5 @@
+package com.constructor.level1;
+
+public class Book {
+
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/BookDriver.java
@@ -0,0 +1,10 @@
+package com.constructor.level1;
+
+public class BookDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
