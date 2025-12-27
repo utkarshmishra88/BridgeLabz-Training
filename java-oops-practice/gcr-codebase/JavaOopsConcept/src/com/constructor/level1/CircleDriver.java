2ec4fe6d510912de15d2916a8d4f2aafe2f14b9e
utkarshmishra88 <utkarshmishra8871@gmail.com>
2025-12-27 22:32:29 +0530

    [Utkarsh] : added circle program and its driver

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/CircleDriver.java
@@ -0,0 +1,16 @@
+package com.constructor.level1;
+
+public class CircleDriver {
+
+	public static void main(String[] args) {
+
+		//initialize default constructor
+		Circle circle1 = new Circle();
+		circle1.display();
+
+		//passing parameters in parameterized constructor
+		Circle circle2 = new Circle(10.5);
+		circle2.display();
+	}
+
+}
