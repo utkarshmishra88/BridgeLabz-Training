a152ec1efa96a86ace1d302997ea74c0bb7d8cfd
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-06 23:43:16 +0530

    [Utkarsh] : added first and last occurence of element

---
+++ java-dsa-practice/gcr-codebase/javaDsaConcepts/src/com/searching/binarysearch/FirstLastOccurrence.java
@@ -0,0 +1,74 @@
+package com.searching.binarysearch;
+import java.util.Scanner;
+
+public class FirstLastOccurrence {
+
+    // Find first occurrence
+    static int findFirst(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;        // store answer
+                right = mid - 1;     // move left
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    // Find last occurrence
+    static int findLast(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;        // store answer
+                left = mid + 1;      // move right
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        int first = findFirst(arr, target);
+        int last = findLast(arr, target);
+
+        if (first == -1) {
+            System.out.println("-1");
+        } else {
+            System.out.println("First Occurrence Index: " + first);
+            System.out.println("Last Occurrence Index: " + last);
+        }
+
+        sc.close();
+    }
+}
