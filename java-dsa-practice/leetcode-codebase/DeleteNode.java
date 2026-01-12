43868a18e5d9dbe0a3b4cb020d7680f07aa9f288
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-12 17:05:26 +0530

    [Utkarsh] : added delete node in a linked-list

---
+++ java-dsa-practice/leetcode-codebase/DeleteNode.java
@@ -0,0 +1,17 @@
+//Delete node in a linkedlist.
+//Leetcode 237
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+      node.val=node.next.val;
+      node.next=node.next.next;
+    }
+}
