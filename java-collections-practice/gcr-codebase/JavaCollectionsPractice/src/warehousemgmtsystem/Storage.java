933e560f505812e1899b369c956461f7ae06ef3a
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-17 00:00:07 +0530

    [Utkarsh] : added smart ware-house management system

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/warehousemgmtsystem/Storage.java
@@ -0,0 +1,15 @@
+package warehousemgmtsystem;
+import java.util.ArrayList;
+import java.util.List;
+
+class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getAllItems() {
+        return items;
+    }
+}
