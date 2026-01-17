b08d88f0db802d4549021d0d3c0e98559cb02f8c
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-17 23:52:56 +0530

    [Utkarsh] : added remove-duplicates with order preserved

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/list/RemoveDuplicatesPreserveOrder.java
@@ -0,0 +1,33 @@
+package com.collections.list;
+import java.util.*;
+
+public class RemoveDuplicatesPreserveOrder {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Set<Integer> seen = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (Integer num : list) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+                result.add(num);
+            }
+        }
+
+        System.out.println("Original List: " + list);
+        System.out.println("After Removing Duplicates: " + result);
+
+        sc.close();
+    }
+}
