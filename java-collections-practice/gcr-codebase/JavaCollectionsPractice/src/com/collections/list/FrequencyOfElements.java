25b2b8443c0ac6ee3bdcd79c29f7b143a147c2ba
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-17 23:49:55 +0530

    [Utkarsh] : added modified file for frequency-count

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/list/FrequencyOfElements.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/list/FrequencyOfElements.java
@@ -5,10 +5,9 @@ public class FrequencyOfElements {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
-        sc.nextLine(); // consume newline
+        sc.nextLine(); //consume newline
 
         List<String> list = new ArrayList<>();
         Map<String, Integer> frequencyMap = new HashMap<>();
@@ -17,8 +16,7 @@ public class FrequencyOfElements {
         for (int i = 0; i < n; i++) {
             String item = sc.nextLine();
             list.add(item);
-
-            // count frequency
+            //count frequency
             frequencyMap.put(item, frequencyMap.getOrDefault(item, 0) + 1);
         }
 
