5cb33a8af107e062d5681664a7b57ad3396e79ad
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-18 13:11:26 +0530

    [Utkarsh] : Implemented check-subset logic

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/set/CheckSubset.java
@@ -0,0 +1,32 @@
+package com.collections.set;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CheckSubset {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        System.out.print("Enter number of elements in Set 1: ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter elements of Set 1:");
+        for (int i = 0; i < n1; i++) {
+            set1.add(sc.nextInt());
+        }
+
+        System.out.print("Enter number of elements in Set 2: ");
+        int n2 = sc.nextInt();
+        System.out.println("Enter elements of Set 2:");
+        for (int i = 0; i < n2; i++) {
+            set2.add(sc.nextInt());
+        }
+
+        // Check subset
+        boolean isSubset = set2.containsAll(set1);
+
+        System.out.println("Is Set1 a subset of Set2? " + isSubset);
+    }
+}
