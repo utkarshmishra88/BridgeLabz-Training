b068db948fd049d6e0b64aee055555cdf4b3fa9d
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-18 13:03:32 +0530

    [Utkarsh] : added union and intersection of set

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/set/UnionIntersectionSets.java
@@ -0,0 +1,38 @@
+package com.collections.set;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UnionIntersectionSets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        System.out.print("Enter number of elements in Set 1: ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter elements of Set 1:");
+        for (int i = 0; i < n1; i++) {
+            set1.add(sc.nextInt());
+        }
+
+        System.out.print("Enter number of elements in Set 2: ");
+        int n2 = sc.nextInt();
+        System.out.println("Enter elements of Set 2:");
+        for (int i = 0; i < n2; i++) {
+            set2.add(sc.nextInt());
+        }
+
+        // Union
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        // Intersection
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        System.out.println("Union: " + union);
+        System.out.println("Intersection: " + intersection);
+    }
+}
