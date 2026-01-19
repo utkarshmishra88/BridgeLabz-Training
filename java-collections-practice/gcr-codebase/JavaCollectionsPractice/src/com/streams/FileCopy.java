2a3118a28f88672f559721bc779a67c32dbd29b3
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-19 23:08:50 +0530

    [Utkarsh] : added copy of one file into another

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/streams/FileCopy.java
@@ -0,0 +1,31 @@
+package com.streams;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+
+    public static void main(String[] args) {
+
+    	String source = "source.txt";
+        String destination = "destination.txt";
+
+        try (FileInputStream fis = new FileInputStream(source);
+             FileOutputStream fos = new FileOutputStream(destination)) {
+
+            int data;
+            while ((data = fis.read()) != -1) {
+                fos.write(data);
+            }
+
+            System.out.println("File copied successfully");
+
+        } catch (IOException e) {
+            System.out.println("Source file not found or I/O error");
+        }
+    }
+}
