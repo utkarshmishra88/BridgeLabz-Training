e3dd2369ca1af24f84726d920f8ce83bf7133992
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-23 15:54:50 +0530

    [Utkarsh] : Implemented Arithmetic and InputMisMatch exception

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exceptionhandling/DivisionExample.java
@@ -0,0 +1,28 @@
+package com.exceptionhandling;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivisionExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter numerator: ");
+            int a = sc.nextInt();
+
+            System.out.print("Enter denominator: ");
+            int b = sc.nextInt();
+
+            int result = a / b;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter numeric values only");
+        }
+
+        sc.close();
+    }
+}
