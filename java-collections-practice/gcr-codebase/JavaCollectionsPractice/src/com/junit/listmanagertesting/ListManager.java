9206ae776aea99cde3f9a9716c113e00b4390fda
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-24 13:27:20 +0530

    [Utkarsh] : Implemented list manager testing

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/junit/listmanagertesting/ListManager.java
@@ -0,0 +1,17 @@
+package com.junit.listmanagertesting;
+import java.util.List;
+
+public class ListManager {
+
+    public void addElement(List<Integer> list, int element) {
+        list.add(element);
+    }
+
+    public void removeElement(List<Integer> list, int element) {
+        list.remove(Integer.valueOf(element)); // remove by value
+    }
+
+    public int getSize(List<Integer> list) {
+        return list.size();
+    }
+}
