0b157ce4d81d3f950e481c9a1a58a86dfb8aa11e
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-26 00:23:27 +0530

    [Utkarsh] : Implemented reflection in dynamic method calling

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/ReflectionDynamicMethodExample.java
@@ -0,0 +1,49 @@
+package com.reflection;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+class MathOperations {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+public class ReflectionDynamicMethodExample {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter method name (add / subtract / multiply): ");
+        String methodName = sc.nextLine();
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        // Load class
+        Class<?> cls = MathOperations.class;
+
+        // Create object
+        Object obj = cls.getDeclaredConstructor().newInstance();
+
+        // Get method dynamically
+        Method method = cls.getMethod(methodName, int.class, int.class);
+
+        // Invoke method
+        Object result = method.invoke(obj, a, b);
+
+        // Display result
+        System.out.println("Result: " + result);
+    }
+}
