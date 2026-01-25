14585b9a4d015610d79b40f4b15f74a7781ae035
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-25 23:38:03 +0530

    [Utkarsh] : Added modified Space-Normalizer

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regex/SpaceNormalizer.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regex/SpaceNormalizer.java
@@ -1,13 +1,18 @@
 package com.regex;
-import java.util.*;
 import java.util.regex.*;
 
+//Normalize multiple spaces to single space
 class SpaceNormalizer{
+	//Main method
 	public static void main(String[] args){
+		//Sample text
 		String text="This   is    an   example   with   multiple    spaces.";
+		//Regex pattern
 		String regex="\\s+";
+		
 		Pattern p=Pattern.compile(regex);
 		Matcher m=p.matcher(text);
+		
 		String result=m.replaceAll(" ");
 		System.out.println(result);
 	}
