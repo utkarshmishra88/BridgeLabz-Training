7147cb7d2a521a30bb715a2b1d8fd0a9e3eb7a75
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-26 17:29:15 +0530

    [Utkarsh] : Validate JSON using Jackson

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreams/src/main/java/com/JavaIOStreams/JSON/practiceproblem/ValidateJsonUsingJackson.java
@@ -0,0 +1,23 @@
+package com.JavaIOStreams.JSON.practiceproblem;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ValidateJsonUsingJackson {
+
+	public static void main(String[] args) {
+
+		// JSON string to validate
+		String json = "{ \"name\": \"Vikash\", \"age\": 22 }";
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			// Try parsing JSON
+			mapper.readTree(json);
+
+			System.out.println("Valid JSON");
+
+		} catch (Exception e) {
+			System.out.println("Invalid JSON");
+		}
+	}
+}
