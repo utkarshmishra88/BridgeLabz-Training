0f2f82ff038162efc0a8873e9eeadb751720826f
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-27 01:03:22 +0530

    [Utkarsh] : added modified file

--- java-iostreams-practice/gcr-codebase/JavaIOStreams/src/main/java/com/JavaIOStreams/JSON/handsonpracticeproblem/ReadJsonFile.java
+++ java-iostreams-practice/gcr-codebase/JavaIOStreams/src/main/java/com/JavaIOStreams/JSON/handsonpracticeproblem/ReadJsonFile.java
@@ -6,8 +6,7 @@ import org.json.JSONObject;
 public class ReadJsonFile {
     public static void main(String[] args) throws Exception {
 
-        String content = new String(Files.readAllBytes(Paths.get("C:\\Users\\dell\\Desktop\\Java-programming-workspace\\java-iostreams-practice\\gcr-codebase\\JavaIOStreams\\src\\main\\java\\com\\JavaIOStreams\\JSON\\handsonpracticeproblem\\data.json"
-        		+ "")));
+        String content = new String(Files.readAllBytes(Paths.get("C:\\Users\\dell\\Desktop\\Java-programming-workspace\\java-iostreams-practice\\gcr-codebase\\JavaIOStreams\\src\\main\\java\\com\\JavaIOStreams\\JSON\\handsonpracticeproblem\\data.json"+ "")));
 
         JSONObject json = new JSONObject(content);
 
