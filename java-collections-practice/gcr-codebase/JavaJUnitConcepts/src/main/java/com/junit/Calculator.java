14307781a6f588a852d0af805b059fbe59b047fd
utkarshmishra88 <utkarshmishra8871@gmail.com>
2026-01-27 14:50:59 +0530

    [Utkarsh] : Added JUnit testing using Maven Project mgmt tool

---
+++ java-collections-practice/gcr-codebase/JavaJUnitConcepts/src/main/java/com/junit/Calculator.java
@@ -0,0 +1,21 @@
+package com.junit;
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        if (b == 0)
+            throw new ArithmeticException("Cannot divide by zero");
+        return a / b;
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaJUnitConcepts/src/main/java/com/junit/FileProcessor.java
@@ -0,0 +1,24 @@
+package com.junit;
+import java.io.*;
+
+public class FileProcessor {
+
+    public void writeToFile(String filename, String content) throws IOException {
+        FileWriter writer = new FileWriter(filename);
+        writer.write(content);
+        writer.close();
+    }
+
+    public String readFromFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+        StringBuilder data = new StringBuilder();
+
+        while ((line = reader.readLine()) != null) {
+            data.append(line);
+        }
+
+        reader.close();
+        return data.toString();
+    }
+}
